5c3030cdfa20bbe91c8032c947370ff96fdca4bd
Ahmed Aboeita <108438941+AhmedAbo-Eita@users.noreply.github.com>
2024-06-24 10:32:00 +0300

    add EX1

--- C_Programing/Unit2_Lesson4_Functions/EX1/main.c
+++ C_Programing/Unit2_Lesson4_Functions/EX1/main.c
@@ -1,2 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
+void prime_numbers(int num1, int num2)
+int main()
+{
+    int in1,in2;
+    printf("Enter two numbers (intervals): ");
+    fflush(stdin);fflush(stdout);
+    scanf("%d %d", &in1, &in2);
+    return 0;
+}
+void prime_numbers(int num1, int num2)
+{
 
+}
