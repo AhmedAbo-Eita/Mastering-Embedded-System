c0c5a6599a579c3328ae0141b458e07c4615021c
AhmedAbo-Eita <ahmedmasre2233@gmail.com>
2024-07-01 03:16:10 +0300

    add ex1 solution

--- C_Programing/Unit2_Lesson4_Functions/EX1/main.c
+++ C_Programing/Unit2_Lesson4_Functions/EX1/main.c
@@ -1,14 +1,48 @@
 #include<stdio.h>
-void prime_numbers(int num1, int num2)
+#include <stdbool.h>
+bool prime_number(int num1);
 int main()
 {
+    //take two numbers from user
     int in1,in2;
     printf("Enter two numbers (intervals): ");
     fflush(stdin);fflush(stdout);
     scanf("%d %d", &in1, &in2);
+    
+    int i = in1;
+    // loop on the values between two numbers add call the fun that check if the num is prim or not
+    for(i; i>=in1 && i<=in2; i++)
+    {
+        if( prime_number(i))
+        {
+            printf("%d\n", i);
+        }
+    }
+   
+    system("pause");
     return 0;
 }
-void prime_numbers(int num1, int num2)
+bool prime_number(int num1)
 {
+    //eleminate the negative numbers and zero
+    if(num1<= 0)
+    {
+        return false;
+    }
+
+    // check if the number is prime number of not
+    int counter =2; // 
+    for(counter; counter*counter<=num1 ; counter++)
+    {
+        if(num1%counter == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
 
 }
