6617c6d5da3babccc5bc5448755e7aa53ad4e475
AhmedAbo-Eita <ahmedmasre2233@gmail.com>
2024-07-01 11:16:08 +0300

    add EX2

--- C_Programing/Unit2_Lesson4_Functions/EX2/main.c
+++ C_Programing/Unit2_Lesson4_Functions/EX2/main.c
@@ -1,8 +1,40 @@
 #include<stdio.h>
+int factorial(int num1);
 
 int main()
 {
-
-
+    int num,result;
+    //take number from user
+    printf("Enter an positive integar: ");
+    fflush(stdin); fflush(stdout);
+    scanf("%d", &num);
+    result = factorial(num);
+    if (num>=0)
+    {
+        printf("Factorial of %d = %d\n", num, result);
+    }
+    else
+    {
+        printf("Can't find the factorial of negative numbers");
+    }
+    
+    
+    //system("pause");
     return 0;
+}
+
+
+int factorial(int num1)
+{
+    
+    if (num1>1)
+    {
+        num1 = num1* factorial(num1-1);
+        return num1;
+    }
+    else
+    {
+        return 1;
+    }
+    
 }
