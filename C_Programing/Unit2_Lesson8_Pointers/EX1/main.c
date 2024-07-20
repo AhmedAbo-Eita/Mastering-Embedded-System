c04a5c59a51e3c987fb68a4e0fbc7a5e1d02e278
AhmedAbo-Eita <ahmedmasre2233@gmail.com>
2024-07-20 23:13:33 +0300

    add pointers ex1

--- C_Programing/Unit2_Lesson8_Pointers/EX1/main.c
+++ C_Programing/Unit2_Lesson8_Pointers/EX1/main.c
@@ -2,7 +2,18 @@
 
 int main()
 {
-        
+    printf("Expected Output :\n");
+    int m = 29;
+    printf("Adress of m : %p \nVlaue of m: %d\n", &m, m);
+    int *ab = &m;
+    printf("Now ab is assigned with the address of m.\n");
+    printf("Adress of pointer ab : %p \nVlaue of pointer ab: %d\n", ab, *ab);
+    m = 34;
+    printf("The value of m assigned to 34 now.\n");
+    printf("Adress of pointer ab : %p \nVlaue of pointer ab: %d\n", ab, *ab);
+    *ab = 7;
+    printf("The pointer variable ab is assigned with the value 7 now.\n");
+    printf("Adress of m : %p \nVlaue of pointer m: %d\n", &m, m);
 
     return 0;
 }
