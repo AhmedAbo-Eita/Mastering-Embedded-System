9ddb5e7aa7c7dde80c7e1db8798a29fe3c52437f
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2024-09-05 17:14:31 +0300

    add unit 3 lesson 1 tasks

---
+++ Unit3_Embedded_C/Lesson1/Led_Toggle/Debug/Src/main.d
@@ -0,0 +1,2 @@
+Src/main.o: ../Src/main.c ../Src/typedef.h
+../Src/typedef.h:
---
+++ Unit3_Embedded_C/Lesson1/Led_Toggle/Src/typedef.h
@@ -0,0 +1,14 @@
+#ifndef TYBEDEF_H
+#define TYPEDEF_H
+
+typedef unsigned long       uint32_t;   //32 bit unsigned integer
+typedef unsigned short      uint16_t;   //16 bit unsigned integer
+typedef unsigned char       uint8_t;    //8 bit unsigned integer
+typedef signed long         int32_t;    //32 bit signed integer
+typedef signed short        int16_t;    //16 bit signed integer
+typedef signed char         int8_t;     //8 bit signed integer
+
+typedef float               float32_t;  //32 bit floating
+typedef double              float64_t;  //64 bit floating
+
+#endif
